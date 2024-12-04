d43132fa081023abdf5010341cc39d766914821a
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2024-12-04 13:35:37 -0800

    client/server component demo

---
+++ app/components/hello.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+function Hello() {
+    console.log("This is a client-side component");
+
+  return (
+    <h1 className="text-3xl">Hello from Hello component</h1>
+  );
+}
+
+export default Hello;
