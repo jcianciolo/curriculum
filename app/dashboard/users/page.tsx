622993c31fd60b977d22c065af0219cc6af46d43
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2024-12-05 13:53:46 -0800

    add: dynamic routing to users

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,14 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
         <h1>Users</h1>
-        <ul>
-            <li>User 1</li>
-            <li>User 2</li>
-            <li>User 3</li>
-            <li>User 4</li>
+        <ul className='mt-10'>
+            <li>
+                <Link href='/dashboard/users/1'>User 1</Link>
+            </li>
+            <li>
+                <Link href='/dashboard/users/2'>User 2</Link>
+            </li>
+            <li>
+                <Link href='/dashboard/users/3'>User 3</Link>
+            </li>
+            <li>
+                <Link href='/dashboard/users/4'>User 4</Link>
+            </li>
         </ul>
     </div>
   )
