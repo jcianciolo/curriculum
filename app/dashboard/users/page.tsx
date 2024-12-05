01b616993c0e0d1cf3b48f4110ccb304c914ad45
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2024-12-05 13:45:44 -0800

    add: dashboard/analytics and dashboard/users routes

---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+        <h1>Users</h1>
+        <ul>
+            <li>User 1</li>
+            <li>User 2</li>
+            <li>User 3</li>
+            <li>User 4</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Page
