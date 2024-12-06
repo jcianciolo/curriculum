5ff5f94cfae8f39afea33c1853501bc02606d6b1
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2024-12-05 21:21:27 -0800

    add: api/books/route.ts

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const Page = () => {
 
-  throw new Error('This is an error')
   return (
     <div>About</div>
   )
---
+++ app/api/db.ts
@@ -0,0 +1,39 @@
+const books = [
+    {
+        id: 1,
+        title: 'The Great Gatsby',
+
+    },
+    {
+        id: 2,
+        title: 'The Catcher in the Rye',
+
+    },
+    {
+        id: 3,
+        title: 'To Kill a Mockingbird',
+
+    },
+    {
+        id: 4,
+        title: '1984',
+
+    },
+    {
+        id: 5,
+        title: 'Pride and Prejudice',
+
+    },
+    {
+        id: 6,
+        title: 'Wuthering Heights',
+
+    },
+    {
+        id: 7,
+        title: 'Jane Eyre',
+
+    },
+];
+
+export default books;
